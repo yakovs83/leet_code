7374c5397614f05dd91580e5b37db7a42d8cd9d5
Sergey Yakovlev <sergeyyak@gmail.com>
2019-07-14 14:42:05 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use leet_code::trie::Trie;
+fn main() {
+    let mut t = Trie::new();
+    t.insert(String::from("dot"));
+    t.insert(String::from("dotty"));
+    t.insert(String::from("drone"));
+}
