e0af34174e0017389cefe37f86365c9ea0fe5967
Sergey Yakovlev <sergeyyak@gmail.com>
2019-09-28 13:01:04 -0600

    Adding a few more solutions

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
+pub mod add_two_numbers;
+pub mod letter_combinations;
+pub mod three_sum;
+pub mod three_sum_closest;
 pub mod trie;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use leet_code::trie::Trie;
+use leet_code::add_two_numbers::*;
+
 fn main() {
-    let mut t = Trie::new();
-    t.insert(String::from("dot"));
-    t.insert(String::from("dotty"));
-    t.insert(String::from("drone"));
+  let res = add_two_numbers(list_from_vec(vec![1, 2, 3]), list_from_vec(vec![9, 8, 9]));
+  println!("The answer is {:?}", res);
 }
