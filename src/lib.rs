117092633430598e6c16520f03356def19db1ec1
Sergey Yakovlev <sergeyyak@gmail.com>
2019-11-10 20:15:31 -0700

    Added more solutions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod add_two_numbers;
+pub mod atoi;
 pub mod length_of_longest_substring;
 pub mod letter_combinations;
 pub mod longest_palindrome;
+pub mod reverse_integer;
 pub mod three_sum;
 pub mod three_sum_closest;
 pub mod trie;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,1 @@
-use leet_code::zigzag_conversion;
-
-fn main() {
-  let res = zigzag_conversion::convert("PAYPALISHIRING".to_owned(), 3);
-  println!("The answer is {:?}", res);
-}
+fn main() {}
