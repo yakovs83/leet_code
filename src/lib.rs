776ff451b5f42b7a36e842b698dfa6a2075db299
Sergey Yakovlev <sergeyyak@gmail.com>
2019-11-16 22:53:41 -0700

    another problem

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod atoi;
 pub mod length_of_longest_substring;
 pub mod letter_combinations;
 pub mod longest_palindrome;
+pub mod palindrome_number;
 pub mod reverse_integer;
 pub mod three_sum;
 pub mod three_sum_closest;
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use leet_code::palindrome_number::is_palindrome;
+fn main() {
+  let input: Vec<i32> = vec![1, 0, -1, 22, 121, 9999, 523, -161];
+  let output: Vec<bool> = input.iter().map(|x| is_palindrome(*x)).collect();
+  input.iter().zip(output.iter()).for_each(|(i, o)| {
+    println!(
+      "{} is {}",
+      i,
+      if *o {
+        "a palindrome"
+      } else {
+        "not a palindrome"
+      }
+    );
+  });
+}
