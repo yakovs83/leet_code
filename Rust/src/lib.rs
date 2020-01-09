86c873770a411302a539bf56c770f4e966f4eb53
Sergey Yakovlev <sergeyyak@gmail.com>
2020-01-08 20:41:27 -0700

    Updating folders to add Scala

--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -4,6 +4,7 @@ pub mod length_of_longest_substring;
 pub mod letter_combinations;
 pub mod longest_palindrome;
 pub mod palindrome_number;
+pub mod regex_matching;
 pub mod reverse_integer;
 pub mod three_sum;
 pub mod three_sum_closest;
---
+++ Rust/src/main.rs
@@ -0,0 +1,11 @@
+//use leet_code::regex_matching::*;
+
+fn main() {
+  // let s = "aab".to_owned();
+  // let p = "c*a*b".to_owned();
+  // let res = is_match(s.clone(), p.clone());
+  // println!(
+  //   "\nString is {:?}\nPattern is {:?}\nResult is {:?}",
+  //   s, p, res
+  // );
+}
